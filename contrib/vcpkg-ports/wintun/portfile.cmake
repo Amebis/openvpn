479d7da631323a9c97fa6c3b133b519c068d37e4
Simon Rozman <simon@rozman.si>
2021-03-16 12:09:18 +0100

    Add Wintun 0.14+ support
    Wintun introduced user-space API in 0.9. With this API declared as
    officially supported, OpenVPN should migrate to it.
    
    However, this commit implements only partial Wintun API support. The
    Wintun API allocates rings non-shareable among processes, rendering
    openvpn.exe and iservice ring exchange impossible.
    
    Nevertheless, the Wintun API was adopted at least for adapter
    management.
    
    Breaking changes:
    
    - --show-adapters reverted to display TAP-Windows6 adapters only (back
      to pre-Wintun era). The show_tap_win_adapters() was not completely
      updated to adopt Wintun in the first place.
    
    - tapctl.exe no longer includes Wintun adapters with TAP-Windows6.
    
    Signed-off-by: Simon Rozman <simon@rozman.si>

---
+++ contrib/vcpkg-ports/wintun/portfile.cmake
@@ -0,0 +1,27 @@
+set(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
+if (VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+    set(WINTUN_ARCH "x86")
+elseif (VCPKG_TARGET_ARCHITECTURE STREQUAL "x64")
+    set(WINTUN_ARCH "amd64")
+elseif (VCPKG_TARGET_ARCHITECTURE STREQUAL "arm64")
+    set(WINTUN_ARCH "arm64")
+else()
+    message(FATAL_ERROR "Unsupported architecture: ${VCPKG_TARGET_ARCHITECTURE}")
+endif()
+
+vcpkg_download_distfile(ARCHIVE
+    URLS https://www.wintun.net/builds/wintun-0.14.1.zip
+    FILENAME wintun-0.14.1.zip
+    SHA512 1b2e393b5ea76236c5204d713955b80aa0f90049152b2a02fe6a4e499bfaf3ee6a8019f8413130c49b0ae29cf6091afec0437a04caa08420d1530466b0a48ff0
+)
+
+vcpkg_extract_source_archive_ex(
+    OUT_SOURCE_PATH SOURCE_PATH
+    ARCHIVE ${ARCHIVE}
+    REF ${7ZIP_VERSION}
+)
+
+file(INSTALL ${SOURCE_PATH}/include/wintun.h DESTINATION ${CURRENT_PACKAGES_DIR}/include)
+file(INSTALL ${SOURCE_PATH}/bin/${WINTUN_ARCH}/wintun.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+file(INSTALL ${SOURCE_PATH}/bin/${WINTUN_ARCH}/wintun.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+file(INSTALL ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/wintun RENAME copyright)
