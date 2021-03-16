baf18d2c1b174dabc92fb0a4ee09d2c5c7b65dd0
Simon Rozman <simon@rozman.si>
2021-03-16 12:09:18 +0100

    Add Wintun 0.14+ support
    Wintun introduced user-space API in 0.9. With this API declared as
    officially supported, OpenVPN should migrate to it.
    
    However, this commit implements only partial Wintun API support. The
    Wintun API allocates rings non-shareable among processes, rendering
    openvpn.exe and iservice ring exchange impossible.
    
    Nevertheless, the Wintun API was adopted at least for adapter
    management.
    
    Breaking changes:
    
    - --show-adapters reverted to display TAP-Windows6 adapters only (back
      to pre-Wintun era). The show_tap_win_adapters() was not completely
      updated to adopt Wintun in the first place.
    
    - tapctl.exe no longer includes Wintun adapters with TAP-Windows6.
    
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- config-msvc.h
+++ config-msvc.h
@@ -120,6 +120,9 @@
 #define PATH_SEPARATOR     '\\'
 #define PATH_SEPARATOR_STR "\\"
 
+/* Prevent inclusion of winsock.h in windows.h. */
+#define _WINSOCKAPI_
+
 #ifndef __cplusplus
 #define inline __inline
 #endif
