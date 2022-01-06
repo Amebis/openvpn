1f0710393601e67087b2b5b8a4f041689de875db
Simon Rozman <simon@rozman.si>
2022-01-06 18:08:12 +0100

    msvc: Statically link everything
    vcpkg and OpenSSL building don't support static RTL with dynamic output.
    It's either all-dynamic or all-static. This prevents us going vc_redist
    independent.
    
    So, let's do static RTL and static output.
    
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- contrib/vcpkg-triplets/x86-windows-ovpn.cmake
+++ contrib/vcpkg-triplets/x86-windows-ovpn.cmake
@@ -1,7 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x86)
 set(VCPKG_CRT_LINKAGE static)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
 
-if(PORT STREQUAL "lz4")
-    set(VCPKG_LIBRARY_LINKAGE static)
+if(PORT STREQUAL "wintun")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
