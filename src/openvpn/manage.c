fe5c915b52d15e2255a2fb7f92aa5326533d02a7
Simon Rozman <simon@rozman.si>
2022-07-12 11:53:59 +0200

    Implement --management-client-pass option
    Management interface password can be passed to the OpenVPN process using
    a password file or stdin. In the immediate future, in eduVPN would like
    to push a complete self-contained .ovpn configuration to the OpenVPN
    process using stdin (i.e. --config stdin) avoiding saving the .ovpn file
    and certs to disk for security concerns.
    
    Therefore, we need to free stdin for --config. Saving management
    password to a file raises same security concerns.
    
    --management-client-pass allows embedding management password inline in
    .ovpn.
    
    Signed-off-by: Simon Rozman <simon@rozman.si>

--- src/openvpn/manage.c
+++ src/openvpn/manage.c
@@ -2451,6 +2451,7 @@ man_settings_init(struct man_settings *ms,
                   const char *addr,
                   const char *port,
                   const char *pass_file,
+                  bool pass_inline,
                   const char *client_user,
                   const char *client_group,
                   const int log_history_cache,
@@ -2473,7 +2474,12 @@ man_settings_init(struct man_settings *ms,
          */
         if (pass_file)
         {
-            get_user_pass(&ms->up, pass_file, "Management", GET_USER_PASS_PASSWORD_ONLY);
+            unsigned int flags = GET_USER_PASS_PASSWORD_ONLY;
+            if (pass_inline)
+            {
+                flags |= GET_USER_PASS_INLINE_CREDS;
+            }
+            get_user_pass(&ms->up, pass_file, "Management", flags);
         }
 
         /*
@@ -2666,6 +2672,7 @@ management_open(struct management *man,
                 const char *addr,
                 const char *port,
                 const char *pass_file,
+                bool pass_inline,
                 const char *client_user,
                 const char *client_group,
                 const int log_history_cache,
@@ -2685,6 +2692,7 @@ management_open(struct management *man,
                       addr,
                       port,
                       pass_file,
+                      pass_inline,
                       client_user,
                       client_group,
                       log_history_cache,
